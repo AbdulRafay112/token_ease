7eba2ddfe586e27c0fe92ea27b75fb17db6c264f
MuhammadArqam005 <arqamahmed005@gmail.com>
2025-12-30 15:42:01 +0500

    try and except rebuild; Add routes: get department and add department

--- backend/app/routes/department_routes.py
+++ backend/app/routes/department_routes.py
@@ -1,33 +1,34 @@
 from fastapi import APIRouter,Request,HTTPException,Depends
+from fastapi.responses import JSONResponse
 from jose import jwt 
 from dotenv import load_dotenv
 import os 
 from database import org_collection
+from schema.department_schema import DepartmentCreate
+from database import dept_collection
 from bson import ObjectId
+from bson import json_util
+import json
 load_dotenv()
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = "HS256"
+
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
 def verify_organization(request:Request):
-        try:
             token = request.cookies.get("access_token")
-            print(token)
             if not token:
                    raise HTTPException(status_code=400,detail="Invalid Token")
-            print(token)
             try:
                 data = jwt.decode(token,SECRET_KEY,algorithms=ALGORITHM)
             except Exception as e:
                 raise HTTPException(status_code=400,detail="Invalid Token")
-            # if data == 'Not enough segments':
-            #     raise HTTPException(status_code=400,detail="Unauthorized Request")
             authorize_user = org_collection.find_one({"_id": ObjectId(data["sub"])})
             if not authorize_user:
                 raise HTTPException(status_code=400,detail="Invalid Token")
             request.state.user_id = authorize_user['_id']
-            # response =  call_next(request)
-            # return response
-        except Exception as e:
-                    raise e
 
 
 
@@ -35,12 +36,37 @@ department_app = APIRouter(prefix="/department",dependencies=[Depends(verify_org
 
 @department_app.get("/")
 def get_department(request:Request):
-        try:
             user_id = request.state.user_id
-            print(user_id)
-            return {"hello" : "department"}
-        except Exception as e:
-              print(e)
-              raise HTTPException(status_code=500,detail="Internal Server Error")
+            departments = dept_collection.find({
+                   "org_id" : user_id
+            })
+            listed_departments = list(departments)
+            if not listed_departments:
+                  raise HTTPException(status_code=200,detail="No Departments Yet")
+            return parse_json(listed_departments)
+        
+
+@department_app.post("/")
+async def add_department(request:Request):
+            body = await request.json()
+            name = body["name"]
+            user_id = request.state.user_id
+            counter_exist = dept_collection.find_one({
+                  "name" : name,
+                  "org_id" : user_id
+            })
+            if counter_exist:
+                  raise HTTPException(status_code=400,detail=f"{name} exists")
+            created_department = dept_collection.insert_one({
+                  "name" : name,
+                  "org_id" : ObjectId(user_id),
+                  "total_tokens" : 0,
+                  "current_token" : 0,
+                  "status" : True
+            })
+            return {
+                  "the department is",str(created_department.inserted_id)
+            }
+
 
 

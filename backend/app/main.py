4edc2342e25b595ac4f7e7642a8b7583fcd4fa3b
AbdulRafay112 <rafayrafay922@gmail.com>
2026-01-17 21:32:16 +0500

    s

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,21 +3,26 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes.organization_routes import router
 from app.routes.department_routes import department_app
 from app.routes.token_routes import token_route
+
 app = FastAPI()
 
 
-
 origins = [
+    "http://localhost:5173", 
     "http://localhost:3000",
-    "https://token-ease-frontend.vercel.app", 
+    "https://token-easefrontend.vercel.app", 
+    "https://token-ease-frontend.vercel.app",
+    "https://token-ease.vercel.app",
+    "https://token-ease-abdulrafay112.vercel.app" 
 ]
+
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,     
-        allow_credentials=True,    
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    CORSMiddleware,
+    allow_origins=origins,     
+    allow_credentials=True,    
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 app.include_router(department_app)
